60fd5a239f938fc1f1f8b365a60147351aa7c4b9
Yokie99 <kyleym@uci.edu>
2024-02-11 14:07:06 -0800

    Colors and ReadMe

---
+++ README_Student.md
@@ -0,0 +1,8 @@
+Kyle Yok Eyh Ma
+2/11/24
+All for One React Rebuild - Recreate a website using all the mini challenge endpoints using React
+Peer Reviewer: Joseph Poncini
+Review: Project is looking good and similar to the figma. I tested all the endpoints and couldn't get any of them to break. Data validation on the endpoints are good. Desktop is good but tablet and mobile could use more break points.  
+
+
+
--- src/component/HomePageComponent.js
+++ src/component/HomePageComponent.js
@@ -13,19 +13,19 @@ export default function HomePage() {
                 <div className="grid grid-cols-3">
                     <div className="flex flex-col justify-center items-center ">
                         <Link to="sayHello">
-                            <button id="sayHelloBtn" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-[4vw] px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+                            <button id="sayHelloBtn" type="button" className="text-white bg-green-400 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-[4vw] px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
             
             border-4 border-black w-[25vw] h-[9vw] my-16  rounded-full"> Say Hello</button>
                         </Link>
 
                         <Link to="AddEm">
-                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+                            <button type="button" className="text-white bg-purple-400 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
             
             border-4 border-black w-32 h-[9vw] sm:w-[25vw]  my-16  rounded-full">Add Em</button>
                         </Link>
 
                         <Link to="WakeUp">
-                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+                            <button type="button" className="text-white bg-teal-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
             
             border-4 border-black w-32 h-[9vw] sm:w-[25vw]  my-16  rounded-full">Wake Up</button>
                         </Link>
@@ -35,20 +35,20 @@ export default function HomePage() {
                     <div className="flex flex-col justify-center items-center ">
 
                         <Link to="LessThanOrGreater">
-                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+                            <button type="button" className="text-white bg-yellow-400 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
             
             border-4 border-black w-32 h-[9vw] sm:w-[25vw]  my-16  rounded-full"> &gt; || &lt;
                             </button>
                         </Link>
                         <Link to="Madlib">
-                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+                            <button type="button" className="text-white bg-orange-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
             
             border-4 border-black w-32 h-[9vw] sm:w-[25vw]  my-16  rounded-full">Madlib
                             </button>
                         </Link>
 
                         <Link to="OddOrEven">
-                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+                            <button type="button" className="text-white bg-pink-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
             
             border-4 border-black w-32 h-[9vw] sm:w-[25vw]  my-16  rounded-full">Odd or Even
                             </button>
@@ -59,21 +59,21 @@ export default function HomePage() {
                     <div className="flex flex-col justify-center items-center ">
 
                         <Link to="Reverse">
-                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+                            <button type="button" className="text-white bg-red-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
             
             border-4 border-black w-32 h-[9vw] sm:w-[25vw]  my-16  rounded-full">Reverse
                             </button>
                         </Link>
 
                         <Link to="Reverse123">
-                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+                            <button type="button" className="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
             
             border-4 border-black w-32 h-[9vw] sm:w-[25vw]  my-16  rounded-full">Reverse 123
                             </button>
                         </Link>
 
                         <Link to="EightBall">
-                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+                            <button type="button" className="text-white bg-green-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
             
             border-4 border-black w-32 h-[9vw] sm:w-[25vw]  my-16  rounded-full">8Ball
                             </button>
@@ -84,7 +84,7 @@ export default function HomePage() {
                     <div className="flex justify-center py-12 ">
 
                         <Link to="Restaurant">
-                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+                            <button type="button" className="text-white bg-purple-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  sm:text-[4vw] text-2xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
             
             border-4 border-black w-32 h-[9vw] sm:w-[25vw]   rounded-full ">Restaurant
                             </button>
